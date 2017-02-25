dccab9b39222be3d143f650abe5cc8ec42e04820
Alexander <shiale14@student.westerdals.no>
2017-02-25 15:39:14 +0100

    add reducer for adding/removing day to/from the ski days array

--- src/index.js
+++ src/index.js
@@ -4,23 +4,33 @@
  */
 
 import Constants from './constants';
-import { errors } from './store/reducers';
+import {allSkiDays} from './store/reducers';
 
 const state = [
-    "user not authorized",
-    "server feed not found"
+    {
+        "resort": "Kirkwood",
+        "date": "2016-12-15",
+        "powder": true,
+        "backcountry": false
+    },
+    {
+        "resort": "Boreal",
+        "date": "2016-12-16",
+        "powder": false,
+        "backcountry": false
+    }
 ];
 
 const action = {
-    type: Constants.CLEAR_ERROR,
-    payload: 0
+    type: Constants.REMOVE_DAY,
+    payload: "2016-12-15"
 };
 
-const nextState = errors(state, action);
+const nextState = allSkiDays(state, action);
 
 console.log(`
 
-    initial state: ${state}
+    initial state: ${JSON.stringify(state)}
     action: ${JSON.stringify(action)}
     new state: ${JSON.stringify(nextState)}
 
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,13 +4,13 @@
  */
 import Constants from '../constants';
 
-export const goal = (state = 10, action) => (action.type === Constants.SET_GOAL) ?  parseInt(action.payload) : state;
+export const goal = (state = 10, action) => (action.type === Constants.SET_GOAL) ? parseInt(action.payload) : state;
 
 export const skiDay = (state = null, action) => (action.type === Constants.ADD_DAY) ? action.payload : state;
 
 export const errors = (state = [], action) => {
 
-    switch(action.type) {
+    switch (action.type) {
 
         case Constants.ADD_ERROR:
             return [...state, action.payload];
@@ -21,5 +21,29 @@ export const errors = (state = [], action) => {
         default:
             return state
     }
+};
+
+export const allSkiDays = (state = [], action) => {
+
+    switch (action.type) {
+
+        case Constants.ADD_DAY:
+
+            const hasDayAlready = state.some(skiDay => skiDay.date === action.payload.date);
+
+            return (hasDayAlready) ?
+                state :
+                [
+                    ...state,
+                    skiDay(null, action)
+                ];
+
+        case Constants.REMOVE_DAY:
+            return state.filter(skiDay => skiDay.date !== action.payload);
+
+        default:
+            return state;
+    }
+
 };
 
